e3cb7b5f9e89145847b5d44386e12a7d855efa74
Alina <goncharenko0206@gmail.com>
2020-11-14 15:52:50 +0200

    added inputmask

--- js/script.js
+++ js/script.js
@@ -13,4 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
         callForm.style.display = "none";
     });
 
+
+
+
+
+    let selector = document.querySelectorAll('input[type="tel"]');
+
+    let im = new Inputmask("+7 (999) 999-99-99");
+    im.mask(selector);
 });
