76293585a911ebfef56401d27efd49227c22d80b
Alina <goncharenko0206@gmail.com>
2020-11-14 19:27:46 +0200

    adde validate form

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
         callForm = document.querySelector(".modal__call"),
         closeForm = document.querySelector(".modal__close");
 
-
+    //modal
     call.addEventListener("click", function (e) {
         callForm.style.display = "block";
         e.preventDefault();
@@ -14,11 +14,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-
-
-
+    //inputmask
     let selector = document.querySelectorAll('input[type="tel"]');
-
     let im = new Inputmask("+7 (999) 999-99-99");
     im.mask(selector);
+
+    //validate forms
+
+    let validateForms = function (selector, rules, succesModal, yaGoal) {
+        new window.JustValidate(selector, {
+            rules: rules,
+            messages: {
+                name: {
+                    minLength: "My custom message about only minLength rule"
+                },
+                tel: "Заполните поле"
+            },
+        });
+    };
+    validateForms(".modal__form", { tel: { required: true } });
+    validateForms(".promo__form", { tel: { required: true } });
+    validateForms(".request__form", { tel: { required: true } });
+
 });
